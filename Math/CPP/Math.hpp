3292fb6f3e74d34309d8a560a265082b17c0c2d5
CliffyA <>
2025-06-12 04:35:36 +1000

    Add Math

---
+++ Math/CPP/Math.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace NumbatLogic
+{
+	class Math
+	{
+		public:
+			static float Abs(float fX);
+			static float Sin(float fAngle);
+			static float Cos(float fAngle);
+			static float Sqrt(float fX);
+			static float Atan2(float fX, float fY);
+	};
+}
+
