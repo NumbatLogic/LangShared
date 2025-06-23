02c09c34a9252c28bd0a79b685eb4cb55ac83978
CliffyA <github@cliffya.com>
2025-06-24 02:32:13 +1000

    test metaprogramming

---
+++ Test/TestMetaprogramming.php
@@ -0,0 +1,43 @@
+<?
+	global $g_sTestArray;
+	$g_sTestArray = array();
+
+	function Test_Add($sTest)
+	{
+		global $g_sTestArray;
+		$g_sTestArray[] = $sTest;
+	}
+
+	function Test_SetFolder($sFolder)
+	{
+		global $g_sTestArray;
+		$g_sTestArray = array();
+
+		$pDirectory = opendir($sFolder);
+			while($sFile = readdir($pDirectory))
+			{
+				if ($sFile != "." && $sFile != ".." && !is_dir($sFolder . "/" . $sFile))
+				{
+					$pPathInfo = pathinfo($sFile);
+					if ($pPathInfo['extension'] == "nll")// && $sFile != "Main.nll")
+						Test_Add($pPathInfo['filename']);
+				}
+			}
+		closedir($pDirectory);
+
+		sort($g_sTestArray);
+	}
+
+	function Test_Output()
+	{	
+		global $g_sTestArray;
+
+		for ($i = 0; $i < sizeof($g_sTestArray); $i++)
+		{
+			$sTest = $g_sTestArray[$i];
+			Output("\tConsole::Log(\"$sTest\");\n");
+			Output("\t$sTest::Run();\n");
+		}
+		Output("\n");
+	}
+?>
