082058b270fcb1da3a5115edcbd8958075eb9e57
CliffyA <github@cliffya.com>
2025-06-24 02:31:28 +1000

    fix namespace

--- Zip/CS/ZipFileInfo.cs
+++ Zip/CS/ZipFileInfo.cs
@@ -1,35 +1,32 @@
 ﻿
-namespace NumberDuck
+namespace NumbatLogic
 {
-    namespace Secret
+    public class ZipFileInfo
     {
-        public class ZipFileInfo
+        private string m_sFileName;
+        private int m_nSize;
+        //private uint m_nCrc32;
+
+        internal ZipFileInfo(string sFileName, int nSize /*, uint nCrc32*/)
         {
-            private string m_sFileName;
-            private int m_nSize;
-            //private uint m_nCrc32;
-
-            internal ZipFileInfo(string sFileName, int nSize /*, uint nCrc32*/)
-            {
-                m_sFileName = sFileName;
-                m_nSize = nSize;
-                //m_nCrc32 = nCrc32;
-            }
-
-            public string GetFileName()
-            {
-                return m_sFileName;
-            }
-
-            public int GetSize()
-            {
-                return m_nSize;
-            }
-
-            /*public uint GetCrc32()
-            {
-                return m_nCrc32;
-            }*/
+            m_sFileName = sFileName;
+            m_nSize = nSize;
+            //m_nCrc32 = nCrc32;
         }
+
+        public string GetFileName()
+        {
+            return m_sFileName;
+        }
+
+        public int GetSize()
+        {
+            return m_nSize;
+        }
+
+        /*public uint GetCrc32()
+        {
+            return m_nCrc32;
+        }*/
     }
 }
