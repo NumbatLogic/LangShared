6cca4493bf5f0f282a7e46389eeae02404c1ab92
CliffyA <github@cliffya.com>
2025-09-21 17:40:17 +1000

    moving transpiled LangShared back into this folder

---
+++ Transpiled/Vector/OwnedVector.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "../../Vector/CPP/Vector.hpp"
+
+namespace NumbatLogic
+{
+	template <class T>
+	class Vector;
+}
+namespace NumbatLogic
+{
+	template <class T>
+	class OwnedVector
+	{
+		protected: Vector<T>* m_pVector;
+		public: OwnedVector()
+		{
+			m_pVector = 0;
+			m_pVector = new Vector<T>();
+		}
+
+		public: virtual ~OwnedVector()
+		{
+			Clear();
+			if (m_pVector) delete m_pVector;
+		}
+
+		public: T PushFront(T xObject)
+		{
+			m_pVector->PushFront(xObject);
+			return m_pVector->Get(0);
+		}
+
+		public: T PushBack(T xObject)
+		{
+			m_pVector->PushBack(xObject);
+			return m_pVector->Get(m_pVector->GetSize() - 1);
+		}
+
+		public: int GetSize()
+		{
+			return m_pVector->GetSize();
+		}
+
+		public: T Get(int nIndex)
+		{
+			return m_pVector->Get(nIndex);
+		}
+
+		public: void Clear()
+		{
+			while (m_pVector->GetSize() > 0)
+			{
+				T pTemp = m_pVector->PopBack();
+				if (pTemp != 0)
+					{
+						delete pTemp;
+						pTemp = 0;
+					}
+			}
+		}
+
+		public: void Insert(int nIndex, T pObject)
+		{
+			m_pVector->Insert(nIndex, pObject);
+		}
+
+		public: T Remove(int nIndex)
+		{
+			T pTemp = m_pVector->Get(nIndex);
+			m_pVector->Erase(nIndex);
+			return pTemp;
+		}
+
+		public: void Erase(int nIndex)
+		{
+			{
+				delete m_pVector->Get(nIndex);
+			}
+			m_pVector->Erase(nIndex);
+		}
+
+		public: T PopBack()
+		{
+			return m_pVector->PopBack();
+		}
+
+		public: T PopFront()
+		{
+			return m_pVector->PopFront();
+		}
+
+	};
+}
+
