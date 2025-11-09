a487d9541e2139105c798b4164a4a4a7a091c6a9
CliffyA <github@cliffya.com>
2025-11-09 14:43:34 +1100

    Gamestrut C# cleanups

--- GameStrutClient/CPP/gsServerSocket.hpp
+++ GameStrutClient/CPP/gsServerSocket.hpp
@@ -5,27 +5,27 @@
 
 namespace NumbatLogic
 {
-    class gsClientSocket;
+	class gsClientSocket;
 	class gsServerSocket : gsSocket
 	{
 		public:
 			gsServerSocket();
 			virtual ~gsServerSocket();
 			
-		    void Start(int port);
-            void Stop();
-            void Update();
-            bool Pending();
-            gsClientSocket* Accept();
+			void Start(int port);
+			void Stop();
+			void Update();
+			bool Pending();
+			gsClientSocket* Accept();
 
-            void Send(gsBlob* pBlob, unsigned int clientSocketId);
+			void Send(gsBlob* pBlob, unsigned int clientSocketId);
 
-        protected:
-            using gsSocket::Send;
-            int m_nSocket;
+		protected:
+			using gsSocket::Send;
+			int m_nSocket;
 			int m_nPort;
-            unsigned int m_nNextClientId;
+			unsigned int m_nNextClientId;
 
-            Vector<gsClientSocket*>* m_pClientSocketVector;
+			Vector<gsClientSocket*>* m_pClientSocketVector;
 	};
 }
