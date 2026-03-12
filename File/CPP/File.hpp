441785c58ac58959232888d0d788eea911be98ee
CliffyA <github@cliffya.com>
2026-03-12 22:25:21 +1100

    More recursive file stuffs

--- File/CPP/File.hpp
+++ File/CPP/File.hpp
@@ -12,6 +12,7 @@ namespace NumbatLogic
 		public: static InternalString* GetContents(const char* sPath);
 		public: static void PutContents(const char* sxPath, const char* sxContents);
 		public: static OwnedVector<InternalString*>* GetRecursiveFileVector(const char* sPath);
+		public: static OwnedVector<InternalString*>* GetRecursiveAllFileVector(const char* sPath);
 
 		public: static InternalString* GetFileDirectory(const char* sxPath);
 		public: static void CreateDirectory(const char* sxPath);
