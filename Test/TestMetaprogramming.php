fb4baeb5509585436f43aa6ce6f15ccffe2eba88
CliffyA <>
2026-03-18 19:26:29 +1100

    Make test generation cooler

--- Test/TestMetaprogramming.php
+++ Test/TestMetaprogramming.php
@@ -1,6 +1,11 @@
 <?
 	global $g_sTestArray;
+	global $g_sTestNamespace;
+	global $g_sTestArgVector;
+
 	$g_sTestArray = array();
+	$g_sTestNamespace = null;
+	$g_sTestArgVector = "sArgVector";
 
 	function Test_Add($sTest)
 	{
@@ -12,6 +17,11 @@
 	{
 		global $g_sTestArray;
 		$g_sTestArray = array();
+
+		global $g_sTestNamespace;
+		$g_sTestNamespace = null;
+		global $g_sTestArgVector;
+		$g_sTestArgVector = "sArgVector";
 
 		$pDirectory = opendir($sFolder);
 			while($sFile = readdir($pDirectory))
@@ -28,18 +38,57 @@
 		sort($g_sTestArray);
 	}
 
+	function Test_SetNamespace($sNamespace)
+	{
+		global $g_sTestNamespace;
+		$g_sTestNamespace = $sNamespace;
+	}
+
+	function Test_SetArgVector($sArgVector)
+	{
+		global $g_sTestArgVector;
+		$g_sTestArgVector = $sArgVector;
+	}
+
+	function Test_DisableArgVector()
+	{
+		global $g_sTestArgVector;
+		$g_sTestArgVector = null;
+	}
+
 	function Test_Output($bNamespace = false)
-	{	
+	{
 		global $g_sTestArray;
+		global $g_sTestNamespace;
+		global $g_sTestArgVector;
 
 		for ($i = 0; $i < sizeof($g_sTestArray); $i++)
 		{
 			$sTest = $g_sTestArray[$i];
-			Output("\tConsole::Log(\"$sTest\");\n");
+
+			$sClassName = "";
+			if ($g_sTestNamespace != null)
+				$sClassName = "$g_sTestNamespace::";
 			if ($bNamespace)
-				Output("\t$sTest::Test::Run();\n");
+				$sClassName .= "$sTest::Test";
 			else
-				Output("\t$sTest::Run();\n");
+				$sClassName .= "$sTest";
+
+			$sIndent = "\t";
+			if ($g_sTestArgVector != null)
+			{
+				Output("\tif ($g_sTestArgVector.GetSize() == 0 || ExternalString::Equal($g_sTestArgVector.Get(0), \"$sTest\"))\n");
+				Output("\t{\n");
+				$sIndent = "\t\t";
+			}
+
+			Output("$sIndent" . "Console::Log(\"$sTest\");\n");
+			Output("$sIndent" . "$sClassName::Run();\n");
+
+			if ($g_sTestArgVector != null)
+			{
+				Output("\t}\n");
+			}
 		}
 		Output("\n");
 	}
