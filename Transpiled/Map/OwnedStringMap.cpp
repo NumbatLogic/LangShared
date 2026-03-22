11f3415c63c1bb3e5a364ee0591937219b1af273
CliffyA <github@cliffya.com>
2026-03-22 22:20:33 +1100

    Missed dis

---
+++ Transpiled/Map/OwnedStringMap.cpp
@@ -0,0 +1,16 @@
+#include "OwnedStringMap.hpp"
+#include "../../Source/Map/CPP/StringMap.hpp"
+
+namespace NumbatLogic
+{
+	template <class TValue>
+	class OwnedStringMap;
+	template <class TValue>
+	class StringMap;
+}
+#line 0 "/home/cliffya/git/LangShared/Source/Map/OwnedStringMap.nll"
+namespace NumbatLogic
+{
+#line 3 "/home/cliffya/git/LangShared/Source/Map/OwnedStringMap.nll"
+}
+
