234ae4ce8c93b51bce15c71c927299192d4f85bb
CliffyA <github@cliffya.com>
2026-03-26 17:13:29 +1100

    More better

--- Script/Rebuild.sh
+++ Script/Rebuild.sh
@@ -6,6 +6,21 @@ PROJECT_NAME="$(basename "$PWD")"
 PROJECT_NAME="${PROJECT_NAME#nll-}"
 TEST_PROJECT_NAME="${PROJECT_NAME}Test"
 
+CODECRAB_SCRIPT="./CodeCrab.sh"
+if [ ! -f "$CODECRAB_SCRIPT" ]; then
+	CODECRAB_SCRIPT="$SCRIPT_DIR/CodeCrab.sh"
+fi
+
+TRANSPILE_SCRIPT="./Transpile.sh"
+if [ ! -f "$TRANSPILE_SCRIPT" ]; then
+	TRANSPILE_SCRIPT="$SCRIPT_DIR/Transpile.sh"
+fi
+
+BUILD_SCRIPT="./Build.sh"
+if [ ! -f "$BUILD_SCRIPT" ]; then
+	BUILD_SCRIPT="$SCRIPT_DIR/Build.sh"
+fi
+
 if [ ! -f "Source/${PROJECT_NAME}.php" ]; then
 	echo "Missing CPP project file: Source/${PROJECT_NAME}.php"
 	exit 1
@@ -19,12 +34,12 @@ if [ -f "Source/LangShared.package-list" ]; then
 	rm -rf "$SCRIPT_DIR/../Transpiled"
 fi
 
-./CodeCrab.sh
-./Transpile.sh
+"$CODECRAB_SCRIPT"
+"$TRANSPILE_SCRIPT"
 
 cd Source
 	php "${PROJECT_NAME}.php" "linux_gmake"
 	php "${PROJECT_NAME}.php" "dotnet10"
 cd ..
 
-./Build.sh "$@"
+"$BUILD_SCRIPT" "$@"
